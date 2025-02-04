2818c872b7be8a26072f93621bc423851babaabf
Bhoomika-NV <bhoomikanv6@gmail.com>
2025-02-04 23:15:32 +0530

    changed the class names with Impl

---
+++ src/main/java/com/example/demo/services/PlaylistServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.PlayList;
+import com.example.demo.repository.PlaylistRepository;
+
+@Service
+public class PlaylistServiceImpl implements playlistService {
+
+	@Autowired
+	PlaylistRepository repo;
+
+	@Override
+	public void addPlaylist(PlayList playlist) {
+		 repo.save(playlist);
+		}
+
+	@Override
+	public List<PlayList> fetchAllPlaylists() {
+ 
+return repo.findAll();
+	}
+}
